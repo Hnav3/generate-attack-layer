904ecea87fa097b357d7b9fa9ed1c72eeacd4570
Evan H <eaholznecht@gmail.com>
2023-01-27 15:17:26 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# generate-attack-layer
+# Generate Attack Layer
+
+## CSV Format
+
+Requires CSV input. Field names must be techID, useCase, dataSource.
+
+## SVG Known issues
+Issue with inconsistent font. 
+https://github.com/mitre-attack/mitreattack-python/issues/89
+https://github.com/mitre-attack/attack-navigator/pull/394
--- generate-layer.py
+++ generate-layer.py
@@ -3,30 +3,58 @@ import argparse
 import csv
 import sys
 
+from mitreattack.navlayers import Layer
+from mitreattack.navlayers import ToSvg, SVGConfig
+
 # Static ATT&CK Navigator layer JSON fields
+ATTACK_VERSION = "12"
 LAYER_VERSION = "4.3"
-NAV_VERSION = "4.0"
-NAME = "LAYERNAME"
-DESCRIPTION = "DESCRIPTION OF LAYER"
+NAV_VERSION = "4.8.0"
+NAME = "ATT&CK Coverage"
+DESCRIPTION = "Coverage of MITRE ATT&CK techniques"
 DOMAIN = "enterprise-attack"
 
 # Base ATT&CK Navigation layer
 layer_json = {
     "versions": {
         "layer": LAYER_VERSION,
-        "navigator": NAV_VERSION
+        "navigator": NAV_VERSION,
+        "attack": ATTACK_VERSION
     },
     "name": NAME,
     "description": DESCRIPTION,
     "domain": DOMAIN,
+    "filters": {
+        "platforms": [
+            "Linux",
+            "macOS",
+            "Windows",
+            "Containers",
+            "Office 365",
+            "SaaS",
+            "Google Workspace",
+            "IaaS",
+            "Azure AD",
+            "Network",
+            "PRE"
+        ]
+    },
+    "layout": {
+        "layout": "side",
+        "showID": True,
+        "showName": True,
+    },
+    "selectTechniquesAcrossTactics": True,
+    "showTacticRowBackground": True,
+    "selectSubtechniquesWithParent": True,
     "techniques": [],
     "gradient": {
         "colors": [
-            "#ffffff",
-            "#ff6666"
+            "#ffffffff",
+            "#66a0ffff"
         ],
         "minValue": 0,
-        "maxValue": 5
+        "maxValue": 1
     }
 }
 
@@ -43,6 +71,7 @@ def arg_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", action="store", dest="input_fn", default="attack.csv",
                         required=True, help="input ATT&CK csv file")
+    parser.add_argument("-s","--svg", action="store_true", dest="to_svg", help="Output layer to svg file.")
 
     return parser 
 
@@ -56,13 +85,21 @@ def main(args):
             else:
                 for t in layer_json["techniques"]:
                     if t["techniqueID"] == row["techID"]:
-                        t["score"] = t["score"]+1
                         metadata = {"name":row["dataSource"],"value":row["useCase"]}
                         t["metadata"].append(metadata)
 
-    
-    with open('layer.json','w') as jsonfile:
-        json.dump(layer_json,jsonfile, indent=4)
+    if args.to_svg == True:
+        lay = Layer()
+        lay.from_dict(layer_json)
+        
+        svg_config = SVGConfig()
+        svg_config.load_from_file(filename='svg_config.json')
+        t = ToSvg(domain=lay.layer.domain, source='local', resource='attack_data/enterprise-attack.json', config=svg_config)
+        t.to_svg(layerInit=lay, filepath = "attack_coverage.svg")
+
+    else: 
+        with open('layer.json','w') as jsonfile:
+            json.dump(layer_json,jsonfile, indent=4)
 
 
 if __name__ == "__main__":
